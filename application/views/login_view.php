aa10070723b9e0daccfd103fe235ab198fd1461e
rehanarroihan <third2014project@gmail.com>
2020-11-04 18:34:17 +0700

    change login logo

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -23,7 +23,7 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="https://aisysaeexpress.com/wp-content/uploads/2020/08/cropped-Logo-192x192.jpg" style="border: 4px solid #f4f6f9;" alt="logo" width="125" class="shadow-dark rounded-circle">
+              <img src="https://aisysaeexpress.com/wp-content/uploads/2020/11/lisasejahtera.png" alt="logo" width="125">
             </div>
 
             <div class="card card-success">
