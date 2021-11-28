357c59f9058a6479a74642b7ae13f0795e6d05b7
rehanarroihan <third2014project@gmail.com>
2021-11-28 21:33:49 +0700

    the one year bug fixed

--- application/models/Shipping_model.php
+++ application/models/Shipping_model.php
@@ -186,6 +186,7 @@ class Shipping_model extends CI_Model {
         $query = $this->db
                     ->where('origin_branch_id', $branchId)
                     ->where('MONTH(shipping.created_at)', Date('m'))
+                    ->where('YEAR(shipping.created_at)', Date('y'))
                     ->order_by('id',"DESC")
                     ->get($this->tableName);
 
